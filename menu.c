a3219c4bcc8fef9048cb338c6a987dbd8212b19a
KOundjibe6074 <pkoundjibe@gmail.com>
2024-04-14 21:36:00 +0100

    La creation du fichier menu.c par KOUNDJIBE PROSPER

---
+++ menu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "menu.h"
+
+void afficherMenu() {
+    printf("\n===== GESTION DE STOCK =====\n");
+    printf("1. Ajouter un produit\n");
+    printf("2. Afficher le stock\n");
+    printf("3. Rechercher un produit\n");
+    printf("4. Modifier un produit\n");
+    printf("5. Supprimer un produit\n");
+    printf("6. Quitter\n");
+    printf("Choix : ");
+}
