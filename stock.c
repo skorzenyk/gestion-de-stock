b5db2c7fe18ad55216c8a2c263450e9987083266
Kemleyogoto skorzeny <zenyskor20@gmail.com>
2024-04-15 11:00:46 +0100

    modification du code pour le rendre compilable sur tous les systeme

--- stock.c
+++ stock.c
@@ -22,7 +22,8 @@ void ajouterProduit(Produit stock[], int *nbProduits) {
 
 void afficherStock(Produit stock[], int nbProduits) {
     printf("\n===== STOCK ACTUEL =====\n");
-    for (int i = 0; i < nbProduits; i++) {
+    int i;
+    for (i = 0; i < nbProduits; i++) {
         printf("Code : %d, Nom : %s, Quantite : %d, Prix : %.2f\n",
                stock[i].code, stock[i].nom, stock[i].quantite, stock[i].prix);
     }
@@ -32,8 +33,8 @@ void rechercherProduit(Produit stock[], int nbProduits) {
     int codeRecherche;
     printf("Entrez le code du produit a rechercher : ");
     scanf("%d", &codeRecherche);
-
-    for (int i = 0; i < nbProduits; i++) {
+    int i;
+    for (i = 0; i < nbProduits; i++) {
         if (stock[i].code == codeRecherche) {
             printf("Produit trouve : Code : %d, Nom : %s, Quantite : %d, Prix : %.2f\n",
                    stock[i].code, stock[i].nom, stock[i].quantite, stock[i].prix);
@@ -48,8 +49,8 @@ void modifierProduit(Produit stock[], int nbProduits) {
     int codeModification;
     printf("Entrez le code du produit a modifier : ");
     scanf("%d", &codeModification);
-
-    for (int i = 0; i < nbProduits; i++) {
+    int i;
+    for (i = 0; i < nbProduits; i++) {
         if (stock[i].code == codeModification) {
             printf("Nouveau nom du produit : ");
             scanf("%s", stock[i].nom);
@@ -69,8 +70,8 @@ void supprimerProduit(Produit stock[], int *nbProduits) {
     int codeSuppression;
     printf("Entrez le code du produit a supprimer : ");
     scanf("%d", &codeSuppression);
-
-    for (int i = 0; i < *nbProduits; i++) {
+    int i;
+    for (i = 0; i < *nbProduits; i++) {
         if (stock[i].code == codeSuppression) {
             for (int j = i; j < *nbProduits - 1; j++) {
                 stock[j] = stock[j + 1];
